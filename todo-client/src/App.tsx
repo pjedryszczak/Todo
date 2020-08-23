e46e6be01a98ba521e5b2573fa5a2c9ebea41e9e
przemyk94 <pjedryszczak@gmail.com>
2020-08-23 17:21:41 +0200

    fix all works now

--- todo-client/src/App.tsx
+++ todo-client/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save fsfstodd reload LALA.
+          Edit <code>src/App.tsx</code> and save fsfstodd reload LALAXXX.
         </p>
         <a
           className="App-link"
